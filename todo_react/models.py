7bb9ba4198a935abf559eba47c920dea01dff6c5
Samuel Coelho <samuelfcmc@gmail.com>
2015-07-06 00:32:28 +0100

    Created requirements file and first models

--- todo_react/models.py
+++ todo_react/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+
+class UserToDos(models.Model)
+    user = models.ForeignKey(User)
+    todos = models.OneToManyField(ToDo)
